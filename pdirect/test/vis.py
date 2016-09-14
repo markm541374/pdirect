978eb9d1ff0d7b58f05150dae0a53fee37d21d87
markm541374 <markm@robots.ox.ac.uk>
2016-09-14 15:53:31 +0100

    plot the pomap

---
+++ pdirect/test/vis.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+
+#strictly for 2d problems
+def drawrect(r,ax,color='b',txt=None):
+    x=float(r[1])/float(r[2])
+    y=float(r[3])/float(r[4])
+    rx=0.5/(3.**r[5])
+    ry = 0.5 / (3. ** r[6])
+    X=[x-rx,x+rx,x+rx,x-rx,x-rx]
+    Y=[y+ry,y+ry,y-ry,y-ry,y+ry]
+    ax.plot(X,Y,color=color)
+    ax.plot(x,y,'.',color=color)
+    ax.text(x, y, txt, fontsize=10)
+
+def drawgrid(G,ax):
+    n=G.shape[0]
+    for i in xrange(n):
+        drawrect(G[i,:],ax,txt=str(i))
+    return
+
+def drawpomap(G,Y,ax):
+    n = G.shape[0]
+    for i in xrange(n):
+        x = 0.5*3.**(-float(G[i,0]))
+        y = Y[i]
+        ax.plot(x,y,'b.')
+        ax.text(x,y, str(i), fontsize=12)
+    mn=min(Y)
+    mx=max(Y)
+    ax.axis([0,0.55,mn-0.1*(mx-mn),mx+0.1*(mx-mn)])
+    return
