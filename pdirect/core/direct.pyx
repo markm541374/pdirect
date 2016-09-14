535024bcc5b8e8945104b9de067d6ca8ce12a212
markm541374 <markm@robots.ox.ac.uk>
2016-09-14 15:54:36 +0100

    add half-length and remove x from getpo1, fix halflegth update in split

--- pdirect/core/direct.pyx
+++ pdirect/core/direct.pyx
@@ -11,22 +11,9 @@ cdef inline double dbl_max(double a, double b): return a if a >= b else b
 cdef inline double dbl_min(double a, double b): return a if a <= b else b
 
 
-cpdef np.ndarray reptof(int[:] rc ,int D, double [:] outv=None):
-
-
-    if outv is None:
-        out=np.empty(D*2)
-        outv=out
-    else:
-        out=None
-    cdef int i
-    for i in range(D):
-        outv[i]=<double>rc[2*i]/<double>rc[2*i+1]
-        outv[D+i]=0.5/<double>(3**rc[D*2+i])
-    return out
 cdef int splitpart2(int[:] r0, int D, int [:,:] G, double [:] Y,int n):
     #nn is the number of div axes, there are 2n entries
-    cdef int i,j,i0
+    cdef int i,j,i0,newc
     cdef double t0,mx
     ym_ = np.empty(n)
     cdef double[:] ym = ym_
@@ -47,13 +34,16 @@ cdef int splitpart2(int[:] r0, int D, int [:,:] G, double [:] Y,int n):
         #split on axis i0
 
         r0[D*2+1+i0]+=1
+        newc=min(r0[D*2+1:])
         G[i0*2,D*2+1:]=r0[D*2+1:]
         G[i0*2+1,D*2+1:]=r0[D*2+1:]
-
+        G[i0*2,0]=newc
+        G[i0*2+1,0]=newc
         #set ym on the split axis high
         ym[i0]=mx
-
+    r0[0]+=1
     return 0
+
 cdef int splitpart1(int[:] rin, int D, int [:,:] G):
     #make new rects on the axes that are going to split and update their centerpoints
     #tail will be the number of new rectangles
@@ -83,7 +73,9 @@ cdef int splitpart1(int[:] rin, int D, int [:,:] G):
             G[tail+1,i*2+2]=d1
             tail+=2
     return tail
+
 cdef int evaluate(f, int [:] ToEv, int nToEv,double[:] lb, double[:] ub, int D, int [:,:] G, double [:] Y):
+    #evaluate a set of nToEv points definded by the rectangles at rows ToEv in G. Values are stored in Y at the corresponding locations
     cdef int i,j,q
     cdef double s
     x = [0.]*D
@@ -94,6 +86,55 @@ cdef int evaluate(f, int [:] ToEv, int nToEv,double[:] lb, double[:] ub, int D, 
             x[j] = lb[j]+s*(ub[j]-lb[j])
             Y[q] = f(x)
     return 0
+
+cdef getporect1(int [:,:] G, double [:] Y, int nbins, int D):
+    #return all min rectangles and their x and y
+    cdef int i,j,k
+    cdef int nr = Y.shape[0]
+
+    #ry and ri are the yvalue and row in G of the min value at each size
+    ry_ = np.empty(nbins,dtype=np.dtype('d'))
+    cdef double[:] ry=ry_
+    ri_ = np.empty(nbins,dtype=np.dtype('i'))
+    cdef int[:] ri=ri_
+
+    for i in range(nbins):
+        ri[i]=-1
+    for i in range(nr):
+        print i
+        j=G[i,0]
+        print j
+        if ri[j]==-1:
+            ri[j]=i
+            ry[j]=Y[i]
+        elif Y[i]<ry[j]:
+            ri[j]=i
+            ry[j]=Y[i]
+        else:
+            pass
+    print [ri_,ry_]
+    cdef int npor = 0
+    for i in range(nbins):
+        if ri[i]>=0:
+            npor+=1
+    #holders for index,x,y only at sizes that have rectangles
+    iout = np.empty(npor,dtype=np.dtype('i'))
+    yout = np.empty(npor,dtype=np.dtype('d'))
+    #xout = np.empty(shape=[npor,D],dtype=np.dtype('d'))
+    rout = np.empty(npor,dtype=np.dtype('d'))
+
+    j=0
+    for i in range(nbins):
+        #print xout
+        if ri[i]>=0:
+            iout[j]=ri[i]
+            yout[j]=ry[i]
+            #for k in range(D):
+            #    xout[j,k]=<double>G[ri[i],1+2*k]/<double>G[ri[i],2+2*k]
+            rout[j]=0.5*3**(-<double>G[ri[i],0])
+            j+=1
+    return iout,yout,rout
+
 
 def direct(f,lower, upper, double vfrac=0.0000001,int maxf=2000):
     #iterator indicies
@@ -128,15 +169,18 @@ def direct(f,lower, upper, double vfrac=0.0000001,int maxf=2000):
     #cdef double [:] sv = s
     #print reptof(G[0,1:],D,sv)
     #print s
+    print G_[:tail,:]
+    print Y_
     m = splitpart1(G[0,:],D,G[1:,:])
+    tail+=m
     nToEv=D*2+1
     for i in range(nToEv):
         ToEv[i]=i
     evaluate(f,ToEv,nToEv,lb,ub,D,G,Y)
-    print G_
+    print G_[:tail,:]
     print Y_
     n = splitpart2(G[0,:],D,G[1:,:],Y[1:],m/2)
-    print G_
-
-    return
+    print G_[:tail,:]
+    print getporect1(G[:tail,:],Y[:tail],gridmax+1,D)
+    return G_[:tail,:],Y_[:tail]
 
